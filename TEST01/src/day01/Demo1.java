8372677db0ee548dc189097fcdb74a168bf4f861
zhangwm123 <1240932100@qq.com>
2019-03-11 14:15:37 +0800

    3月11日下午提交

---
+++ TEST01/src/day01/Demo1.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+		System.out.println("今天天气真的不好");
+	}
+
+}
